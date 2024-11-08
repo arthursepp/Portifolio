838c4010df86726450502ff060ad5fe6c6cdc665
Arthur Souza Sepp <arthursouzasepp@apoioprodesp.sp.gov.br>
2024-11-08 11:52:20 -0300

    Iniciando desenvolvimento do portal de filmes

--- portal-filmes/app/admin.py
+++ portal-filmes/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Perfil)
+admin.site.register(Filme)
+admin.site.register(Serie)
+admin.site.register(Poster)
+admin.site.register(Ator)
+admin.site.register(Genero)
+admin.site.register(Diretor)
+admin.site.register(ProdutorSerie)
+admin.site.register(Comentario)
