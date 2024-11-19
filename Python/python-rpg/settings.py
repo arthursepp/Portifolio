32477b9ca7700b1f843fd8a10e06eb0f71cdfa30
Arthur Souza Sepp <arthursouzasepp@apoioprodesp.sp.gov.br>
2024-11-19 10:07:40 -0300

    Adicionando armas

--- Python/python-rpg/settings.py
+++ Python/python-rpg/settings.py
@@ -3,3 +3,11 @@ WIDTH = 1280  # Largura da janela do jogo
 HEIGHT = 720  # Altura da janela do jogo
 FPS = 60  # Frames por segundo
 TILESIZE = 64  # Tamanho dos tiles
+
+weapon_data = {
+    'sword': { 'cooldown': 100, 'damage': 15, 'graphic': 'Python/python-rpg/graphics/weapons/sword/full.png' },
+    'lance': { 'cooldown': 400, 'damage': 30, 'graphic': 'Python/python-rpg/graphics/weapons/lance/full.png' },
+    'axe': { 'cooldown': 300, 'damage': 20, 'graphic': 'Python/python-rpg/graphics/weapons/axe/full.png' },
+    'rapier': { 'cooldown': 50, 'damage': 8, 'graphic': 'Python/python-rpg/graphics/weapons/rapier/full.png' },
+    'sai': { 'cooldown': 80, 'damage': 10, 'graphic': 'Python/python-rpg/graphics/weapons/sai/full.png' },
+}
