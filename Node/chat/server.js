cd64f189b44e9dd6c3e220ca87e97ef1da4ec8e0
Sepp <arthursouzasepp@gmail.com>
2025-02-04 11:44:53 -0300

    Ajustando o design da aplicação de chat e adicionando funcionalidade de nome de usuário

--- Node/chat/server.js
+++ Node/chat/server.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
